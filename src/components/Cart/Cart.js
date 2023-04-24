c35b8b5fc1ed109255d9b737dd3c535d199236d0
KrisZivojevic <kristina.zivojevic@gmail.com>
2023-04-24 17:38:43 +0200

    cart component design

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,22 +1,46 @@
 import React, { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
+import classes from "./Cart.module.css";
 
 const Cart = () => {
   const { cart, deleteProduct, resetCart } = useContext(CartContext);
 
   return (
-    <div>
+    <div className={classes.cart__wrapper}>
       <h3>My Products</h3>
-      {cart.map((product) => {
-        return (
-          <p style={{ marginTop: "80px" }} key={product.id}>
-            {product.title} Quantity: {product.quantity}{" "}
-            <button onClick={() => deleteProduct(product.id)}>X</button>
-          </p>
-          // <Product key={product.id} id={product.id} image={product.image} category={product.category} title={product.title} price={product.price} />
-        );
-      })}
-      <button onClick={resetCart}>Reset Cart</button>
+      <div className={classes.cart__header}>
+        <p></p>
+        <p>Product</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p></p>
+      </div>
+      <div className={classes.cart__products_container}>
+        {cart.map((product) => {
+          return (
+            <div className={classes.cart__product_container} key={product.id}>
+              <img
+                src={product?.image}
+                alt="product"
+                className={classes.cart__image}
+              />
+              <p>{product.title}</p>
+              <p>$ {product.price}</p>
+              <p>{product.quantity}</p>
+              <div className={classes.cart__button_wrapper}>
+                <button
+                  className={classes.cart__button}
+                  onClick={() => deleteProduct(product.id)}
+                >
+                  X
+                </button>
+              </div>
+            </div>
+            // <Product key={product.id} id={product.id} image={product.image} category={product.category} title={product.title} price={product.price} />
+          );
+        })}
+      </div>
+      <button className={classes.cart__reset} onClick={resetCart}>Reset Cart</button>
     </div>
   );
 };
